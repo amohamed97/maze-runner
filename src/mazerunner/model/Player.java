cfdbc320cb43254da3c0ba8129ac97c5a46e7955
OmaRosh <odteam12@gmail.com>
2018-05-12 19:42:33 +0200

    Movement sprites 2

--- src/mazerunner/model/Player.java
+++ src/mazerunner/model/Player.java
@@ -4,15 +4,31 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.beans.property.DoubleProperty;
+import javafx.scene.image.Image;
 import javafx.util.Duration;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
 
 public class Player extends Cell {
     int health = 100;
     int ammo = 6;
     int score = 0;
+    private static Image playerStand;
+    private static Image playerMove;
+
+    static{
+        try {
+            playerStand = new Image(new FileInputStream(Paths.get("images", "playerGun.png").toFile()));
+            playerMove = new Image(new FileInputStream(Paths.get("images", "movement.gif").toFile()));
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
 
     public Player(int row, int col) {
-        super(row, col, "player.png");
+        super(row, col, "playerGun.png");
     }
 
     void fillHealth(){
@@ -28,18 +44,22 @@ public class Player extends Cell {
     }
 
     void playAnimation(DoubleProperty prop, int val){
+        setImage(playerMove);
         final Timeline timeline = new Timeline();
         timeline.setAutoReverse(true);
         timeline.getKeyFrames().add(new KeyFrame(Duration.millis(100), new KeyValue(prop, val)));
         timeline.play();
+        timeline.setOnFinished(e-> setImage(playerStand));
     }
 
     public void setRow(int row) {
+        setRotate(this.row < row? 180 : 0);
         this.row = row;
         playAnimation(yProperty(), row*20);
     }
 
     public void setCol(int col){
+        setRotate(this.col < col? 90 : 270);
         this.col = col;
         playAnimation(xProperty(), col*20);
     }
